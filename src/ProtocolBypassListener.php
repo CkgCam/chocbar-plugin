8d91f26791015dd1470ba8244c867409ce452dd9
CkgCam <34908523+CkgCam@users.noreply.github.com>
2025-03-27 18:28:43 +1300

    dirty fix to support newer mcpe

--- src/ProtocolBypassListener.php
+++ src/ProtocolBypassListener.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 namespace ckgcam\chocbar;
 
 use pocketmine\event\Listener;
-use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\event\server\PacketReceiveEvent;
 use pocketmine\network\mcpe\protocol\LoginPacket;
 use pocketmine\network\mcpe\protocol\ProtocolInfo;
 
 class ProtocolBypassListener implements Listener {
 
-    public function onPacketReceive(DataPacketReceiveEvent $event): void {
+    public function onPacketReceive(PacketReceiveEvent $event): void {
         $packet = $event->getPacket();
 
         if ($packet instanceof LoginPacket) {
@@ -19,7 +19,7 @@ class ProtocolBypassListener implements Listener {
                 786
             ];
 
-            if (in_array($packet->protocol, $allowedProtocols)) {
+            if (in_array($packet->protocol, $allowedProtocols, true)) {
                 $packet->protocol = ProtocolInfo::CURRENT_PROTOCOL;
             }
         }
